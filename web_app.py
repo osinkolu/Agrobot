7abd6437786639587a5c567fe0179b891e3e1049
Osinkolu <femiosinkolu@gmail.com>
2022-05-30 21:52:48 +0100

    minor

--- web_app.py
+++ web_app.py
@@ -239,7 +239,7 @@ def home():
 
 #################### Customer Support Page #############################################################
 def customer_support():
-    help.header("Let Us Help You")
+    help.header("Get Help from Experts")
     help.sub_text("Feel free to make your complaints here: Both Agricultural & Technical support are avialble")
     with st.form(key = 'form1', clear_on_submit=True):
         first_name = st.text_input("Firstname")
